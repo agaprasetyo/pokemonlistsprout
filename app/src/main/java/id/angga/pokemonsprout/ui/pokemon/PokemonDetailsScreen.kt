6a2105f18fcfb0ee87264eb1345045e626092fc9
anggaprasetiyo <agapras@gmail.com>
2024-12-26 17:12:55 +0700

    initial detail screen

---
+++ app/src/main/java/id/angga/pokemonsprout/ui/pokemon/PokemonDetailsScreen.kt
@@ -0,0 +1,13 @@
+package id.angga.pokemonsprout.ui.pokemon
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AnimatedContentScope.PokemonDetailsScreenRoute(
+    viewModel: PokemonListViewModel,
+    detailsViewModel: PokemonDetailsViewModel,
+    onBackClick: (Int) -> Unit,
+) {
+
+}
